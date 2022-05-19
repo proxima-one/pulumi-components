af81dbce75aea8efb86b5ff6bb9f51d494a736c9
OS-M <manalog44@gmail.com>
2022-05-19 15:56:53 +0300

    added mongoExpress

--- packages/proxima-node/src/components/index.ts
+++ packages/proxima-node/src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./dockerRegistry";
 export * from "./kafka";
 export * from "./minio";
 export * from "./mongodb";
+export * from "./mongo-express";
 export * from "./proxima-apps";
 export * from "./proxima-services";
 export * from "./cert-manager";
