56496443605b810ca8279fbf164883025da656ec
OS-M <manalog44@gmail.com>
2022-05-19 15:59:04 +0300

    added mongoExpress.ts

---
+++ packages/proxima-node/src/components/mongo-express/mongoExpress.ts
@@ -0,0 +1,116 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+import * as helpers from "../../helpers";
+
+import {
+  Password,
+  ResourceRequirements,
+} from "../types";
+
+export interface MongoExpressArgs {
+  namespace: pulumi.Input<string>;
+  resources?: ResourceRequirements;
+
+  mongoAdminAuth: MongoExpressDbAuth;
+  auth?: MongoExpressAuth;
+  publicHost?: pulumi.Input<string>;
+  storage: Storage;
+}
+
+export interface MongoExpressDbAuth {
+  username: string;
+  password: string;
+}
+
+export interface MongoExpressAuth {
+  username: string;
+  password: Password;
+}
+
+export class MongoExpress extends pulumi.ComponentResource {
+
+  public readonly chart: k8s.helm.v3.Chart;
+
+  public readonly resolvedPasswords: pulumi.Output<Record<string, string>>;
+  public readonly ingress?: k8s.networking.v1.Ingress;
+
+  public constructor(
+    name: string,
+    args: MongoExpressArgs,
+    opts?: pulumi.ComponentResourceOptions
+  ) {
+    super("proxima-k8s:MongoExpress", name, args, opts);
+
+    const passwords = new helpers.PasswordResolver(this);
+    const auth: MongoExpressAuth = args.auth ?? {
+      username: "mongo-express",
+      password: {
+        type: "random",
+        name: `${name}-secret`,
+      },
+    };
+
+    this.chart = new k8s.helm.v3.Chart(
+      name,
+      {
+        fetchOpts: {
+          repo: "https://cowboysysop.github.io/charts/",
+        },
+        chart: "mongo-express",
+        version: "2.6.5",
+        namespace: args.namespace,
+        values: {
+          service: {
+            port: 80
+          },
+          mongodbEnableAdmin: true,
+          mongodbAdminUsername: args.mongoAdminAuth.username,
+          mongodbAdminPassword: args.mongoAdminAuth.password,
+          basicAuthUsername: auth.username,
+          basicAuthPassword: passwords.resolve(auth.password)
+        },
+      },
+      { parent: this }
+    );
+
+    const svcName = `${name}-mongo-express`;
+
+    if (args.publicHost) {
+      this.ingress = new k8s.networking.v1.Ingress(
+        `${name}`,
+        {
+          metadata: {
+            namespace: args.namespace,
+            annotations: helpers.ingressAnnotations({
+              certIssuer: "letsencrypt",
+              backendGrpc: true,
+              sslRedirect: true,
+              bodySize: "300m",
+            }),
+          },
+          spec: helpers.ingressSpec({
+            host: args.publicHost,
+            path: "/",
+            backend: {
+              service: {
+                name: svcName,
+                port: 80,
+              },
+            },
+            tls: {
+              secretName: `${svcName}-tls`,
+            },
+          }),
+        },
+        { parent: this }
+      );
+    }
+
+    this.resolvedPasswords = passwords.getResolvedPasswords();
+
+    this.registerOutputs({
+      username: auth.username,
+      resolvedPasswords: this.resolvedPasswords,
+    });
+  }
+}
