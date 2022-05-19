fd2fda4c3da283539de1dafeb9286abca348839d
OS-M <manalog44@gmail.com>
2022-05-19 16:05:24 +0300

    removed storage field from MongoExpressArgs

--- packages/proxima-node/src/components/mongo-express/mongoExpress.ts
+++ packages/proxima-node/src/components/mongo-express/mongoExpress.ts
@@ -14,7 +14,6 @@ export interface MongoExpressArgs {
   mongoAdminAuth: MongoExpressDbAuth;
   auth?: MongoExpressAuth;
   publicHost?: pulumi.Input<string>;
-  storage: Storage;
 }
 
 export interface MongoExpressDbAuth {
