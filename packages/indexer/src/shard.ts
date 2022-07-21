01f8b0d3eeacfc05eea01a8caf247a5946427e58
OS-M <manalog44@gmail.com>
2022-07-21 19:15:24 +0300

    multiple consumer support

--- packages/indexer/src/shard.ts
+++ packages/indexer/src/shard.ts
@@ -12,7 +12,7 @@ export interface Shard {
 }
 
 export interface ShardResources {
-  consumer: ShardDeploymentConfig
+  consumers: ShardDeploymentConfig[]
   server?: ShardDeploymentConfig
   storage?: ShardStorageResources
 }
