a4b8330ca403248535cae2c3d5042d260f9b2953
OS-M <manalog44@gmail.com>
2022-08-08 11:47:18 +0200

    added StackNameToEnvName

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,4 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+
 export * from "./indexerDeployment";
 export * from "./indexerMongo";
 export * from "./ingress";
 export * from "./shard";
+
+export function StackNameToEnvName(s: pulumi.Input<string>): pulumi.Input<string> {
+  return pulumi.output(s).apply(str => {
+    switch (str) {
+      case "amur":
+        return "prod"
+      case "amur-dev":
+        return "dev"
+      case "amur-qa":
+        return "qa"
+      default:
+        return "undefined"
+    }
+  })
+}
