6518513606ef1cd7e096e0ded56ed84d1fb1a9b9
Chase Smith <chasesmith@berkeley.edu>
2022-08-18 05:49:47 -0700

    Image tag include services

--- packages/proxima-node/src/components/streamdb/index.ts
+++ packages/proxima-node/src/components/streamdb/index.ts
@@ -27,7 +27,7 @@ export interface StreamDBConnectionDetails {
   endpoints: string[];
 }
 
-const defaultImageTag = "stream-db-0.1.2-303277c";
+const defaultImageTag = "services:stream-db-0.1.2-303277c";
 const appPort = 50051;
 const metricsPort = 2112;
 /**
