8262c2dc5939fe2fac8c013d17f337042f4928c2
OS-M <manalog44@gmail.com>
2022-08-30 15:58:53 +0200

    index test

--- packages/k8s-cluster/src/index.ts
+++ packages/k8s-cluster/src/index.ts
@@ -1,9 +1,1 @@
-export * as abstractions from "./abstractions";
-export * as certManager from "./cert-manager";
-export * as grafana from "./grafana";
 export * as ingressNginx from "./ingress-nginx";
-export * as loki from "./loki";
-export * as monitoringStack from "./monitoring-stack";
-export * as oauth2 from "./oauth2";
-export * as prometheus from "./prometheus";
-export * as utils from "./utils";
