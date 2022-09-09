7db34086e337726ced29dd2392a6d9d8d884f820
OS-M <manalog44@gmail.com>
2022-09-09 13:20:17 +0200

    added status ports

--- packages/service-apps/src/deployers/indexingService.ts
+++ packages/service-apps/src/deployers/indexingService.ts
@@ -155,6 +155,8 @@ export class IndexingServiceDeployer extends AppDeployerBase {
         const env = pulumi.all({
           MODE: app.mode ?? "live",
           METRICS_PORT: "2112",
+          STATUS_GRPC_PORT: "26000",
+          STATUS_HTTP_PORT: "9090",
         });
 
         const consumerEnv: Record<string, string> = {
@@ -211,6 +213,14 @@ export class IndexingServiceDeployer extends AppDeployerBase {
                   name: "http-metrics",
                   containerPort: 2112,
                 },
+                {
+                  name: "http-status",
+                  containerPort: 9090,
+                },
+                {
+                  name: "grpc-status",
+                  containerPort: 26000,
+                },
               ],
             },
             server: {
@@ -225,6 +235,14 @@ export class IndexingServiceDeployer extends AppDeployerBase {
                 {
                   name: "http-metrics",
                   containerPort: 2112,
+                },
+                {
+                  name: "http-status",
+                  containerPort: 9090,
+                },
+                {
+                  name: "grpc-status",
+                  containerPort: 26000,
                 },
                 {
                   name: "http",
